d59a5504d6c257896229fba888ab5fab56a23d1e
210-14\210-14 <210>
2022-03-31 21:02:52 +0500

    31,03,2022

--- src/panels/Home.js
+++ src/panels/Home.js
@@ -1,19 +1,39 @@
-import React, {useState, Fragment} from 'react';
+import React, {useState, Fragment, useEffect, useRef} from 'react';
 import PropTypes from 'prop-types';
 
 import { Panel, PanelHeader, Header, Button, Group, Cell, Div, Avatar, CardGrid,ContentCard, FixedLayout, Separator,WriteBar, WriteBarIcon } from '@vkontakte/vkui';
 
+import io from 'socket.io-client'
 
+const SERVER_URL = 'http://localhost:5000'
 
-
-const Home = ({ id, go, fetchedUser }) => {
+const Home = ({ id, go, fetchedUser, messages, sendMessage  }) => {
 	const [text, setText] = useState(""); 
+
+
+	
+	const handleChangeText = (e) => {
+		setText (e.target.value)
+	}
+
+	const handleSendMessage = (e) => {
+		e.preventDefault ()
+		const trimmed = text.trim()
+		if (trimmed)
+		{
+			sendMessage ({
+				messageText: text, 
+				senderName:`${fetchedUser.first_name} ${fetchedUser.last_name}`,	
+			})
+			setText ('')
+		}
+	}
 
 	return (
 	<Panel id={id}>
 
 
-		<PanelHeader>Чатик</PanelHeader>
+		<PanelHeader>Чат</PanelHeader>
 		
 		 {/*fetchedUser &&
 		<Group header={<Header mode="secondary">Добро пожаловать!</Header>}>
@@ -36,55 +56,37 @@ const Home = ({ id, go, fetchedUser }) => {
 
 		<Group>
 		<CardGrid size="l">
-		  <ContentCard
-			subtitle="VKUI"
-			header="ContentCard example"
-			caption="VKUI Styleguide > Blocks > ContentCard"
-		  />
-		  <ContentCard
-			onClick={() => {}}
-			src="https://images.unsplash.com/photo-1603988492906-4fb0fb251cf8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1600&q=80"
-			subtitle="unsplash"
-			header="brown and gray mountains under blue sky during daytime photo"
-			text="Mountain changji"
-			caption="Photo by Siyuan on Unsplash"
-			maxHeight={150}
-		  />
-		  <ContentCard
-			disabled
-			src="https://images.unsplash.com/photo-1603928726698-a015a1015d0e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=80"
-			subtitle="unsplash"
-			header="persons left hand with pink paint"
-			text="Five hours of makeup and paint to achieve the human anatomy photoshoot. Thank you Steph and Shay. See more and official credit on @jawfox.photography."
-			caption="Photo by Alexander Jawfox on Unsplash"
-			maxHeight={500}
-		  />
+			{messages.map((msg)=>(
+			<ContentCard
+			src={msg.avatar}
+			header={msg.senderName}
+			text={msg.messageText}
+			caption={msg.createdAt}
+			/>
+			))}
+			</CardGrid>
+        </Group>
 
           <FixedLayout vertical="bottom">
         
             <Separator wide />
-
            
             <WriteBar
             
               after={
                 <Fragment>
-				  <WriteBarIcon mode="send" />
+				  <WriteBarIcon mode="send" 
+				  onClick = {handleSendMessage} />
+
                 </Fragment>
               }
               value={text}
-              onChange={(e) => setText(e.target.value)}
-           
+              onChange={handleChangeText}
+          
               placeholder="Сообщение"
             />
           
-        </FixedLayout>
-
-		</CardGrid>
-	  </Group>
-
-
-
+        </FixedLayout>		
 	</Panel> )
 
 };
@@ -103,4 +105,7 @@ Home.propTypes = {
 };
 
 export default Home;
+
+
+
 
